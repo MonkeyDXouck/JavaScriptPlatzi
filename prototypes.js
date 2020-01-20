722be6b0740ca221ba270c6697f2fd6b29327be7
MonkeyDXouck <julian.leo@outlook.es>
2020-01-20 16:54:18 -0500

    Prototypes

--- prototypes.js
+++ prototypes.js
@@ -7,18 +7,24 @@ class Person {
      * @param lastName
      * @param height
      */
-    constructor (name, lastName, height) {
+    constructor (name, lastName, height, isDev) {
         this.name = name;
         this.lastName = lastName;
         this.height = height;
+        this.isDev = isDev;
     }
 
-    greet () {
-        console.dir(`Hola, me llamo ${this.name} ${this.lastName}`)
+    greet (fn) {
+        let { name, lastName, isDev } = this;
+        console.dir('---------------------');
+        console.dir(`Hola, me llamo ${name} ${lastName}`);
+        if (fn) {
+            fn(name, lastName, isDev)
+        }
     }
 
     beTall () {
-        if (this.height > 1.80) {
+        if (this.height > 1.71) {
             console.dir("Soy alto");
         } else {
             console.dir("Soy bajo");
@@ -30,22 +36,36 @@ class Person {
  * Prototype son
  */
 class Development extends Person {
-    constructor (name, lastName, height) {
-        super(name, lastName, height);
+    constructor (name, lastName, height, isDev) {
+        super(name, lastName, height, isDev);
     }
 
-    greet () {
-        console.dir(`Hola, me llamo ${this.name} ${this.lastName} y soy desarrollador/a`)
+    greet (fn) {
+        let { name, lastName, isDev } = this;
+        console.dir('---------------------');
+        console.dir(`Hola, me llamo ${name} ${lastName} y soy desarrollador/a`)
+        if (fn) {
+            fn(name, lastName, isDev)
+        }
+    }
+}
+
+function responseGreet (name, lastName, isDev) {
+    console.dir(`Buen día ${name} ${lastName}`);
+    if (isDev) {
+        console.log('Ah mirá, no sabía que eras desarrollador/a');
     }
 }
 
 function callGreet () {
-    let mack = new Person("Mack", "S.", 1.80);
+    let mack = new Person("Mack", "S.", 1.85, false);
     mack.greet();
-    mack.beTall();
-    let xouck = new Development("Xouck", "D.", 1.70);
-    xouck.greet();
-    xouck.beTall();
+    // mack.beTall();
+    let Kevin = new Person("Kevin", "R.", 1.75, false);
+    Kevin.greet(responseGreet);
+    // xouck.beTall();
+    let xouck = new Development("Xouck", "D.", 1.70, true);
+    xouck.greet(responseGreet);
 }
 
 callGreet();
